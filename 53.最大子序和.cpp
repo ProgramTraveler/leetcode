49e018cfb4ca34463707eb17ab9a38e67c064af5
snake-ming <2115655391@qq.com>
2020-09-09 18:10:27 +0800

    update

--- 53.最大子序和.cpp
+++ 53.最大子序和.cpp
@@ -7,8 +7,23 @@
 // @lc code=start
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-
+    int maxSubArray(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        int MaxSum=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            dp[i]=max(nums[i]+dp[i-1],nums[i]);
+            MaxSum=A(dp[i],MaxSum);
+        }
+        return MaxSum;
+    }
+    int A(int a,int b)
+    {
+        if(a>b) return a;
+        else return b;
     }
 };
 // @lc code=end
