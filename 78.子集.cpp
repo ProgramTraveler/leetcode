00a63e10bee8561443eb2e77f56be26b2170af03
snake-ming <2115655391@qq.com>
2020-09-20 19:22:33 +0800

    update

--- 78.子集.cpp
+++ 78.子集.cpp
@@ -14,10 +14,10 @@ public:
             answer.push_back(tem_ans); //把临时的答案传入临时答案
             return;
         }
-        //把选择的数传入临时数组，并搜索下一个数
+        //把选择的数传入临时数组
         tem_ans.push_back(nums[index]);
         DFS(nums,answer,tem_ans,index+1);
-        //当把全部的数都选择完了，也就是集合本身被搜索完了，就开始取数
+        //不把当前的数放入数组
         tem_ans.pop_back();
         DFS(nums,answer,tem_ans,index+1);
     }
