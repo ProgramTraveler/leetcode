ae0bfd031443824dd43357709bcd17ae6817a339
snake-ming <2115655391@qq.com>
2020-09-24 20:10:31 +0800

    update

---
+++ 43.字符串相乘.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=43 lang=cpp
+ *
+ * [43] 字符串相乘
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n1=num1.size();
+        int n2=num2.size();
+        string res(n1+n2,'0');
+        for(int i=n2-1;i>=0;i--){
+            for(int j=n1-1;j>=0;j--){
+                int temp=(res[i+j+1]-'0')+(num1[j]-'0')*(num2[i]-'0');
+                res[i+j+1]=temp%10+'0';//当前位
+                res[i+j]+=temp/10; //前一位加上进位，res[i+j]已经初始化为'0'，加上int类型自动转化为char，所以此处不加'0'
+            }
+        }
+        
+//去除首位'0'
+        for(int i=0;i<n1+n2;i++){
+            if(res[i]!='0')
+                return res.substr(i);
+        }
+        return "0";
+       
+        
+    }
+};
+
+// @lc code=end
+
