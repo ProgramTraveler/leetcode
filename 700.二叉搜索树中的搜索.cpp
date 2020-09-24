90739556c66417f516b854cab0d5fd55a419c27a
snake-ming <2115655391@qq.com>
2020-09-24 20:41:51 +0800

    update

---
+++ 700.二叉搜索树中的搜索.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=cpp
+ *
+ * [700] 二叉搜索树中的搜索
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) 
+    {
+        if(root==NULL) return NULL; //为空，说明没有匹配的数，直接返回NULL
+
+        if(root->val<val) return searchBST(root->right,val); //如果当前数比给定值小，就去它的右子树搜索
+        else if(root->val>val) return searchBST(root->left,val);//如果当前数比给定值大，就去它的左子树搜索
+
+        else return root; //相等，就返回该节点
+    }
+};
+// @lc code=end
+
