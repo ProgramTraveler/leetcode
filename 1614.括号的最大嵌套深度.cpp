582bf55944b955ebfabc9f57be0c77460cab4e13
snake-ming <2115655391@qq.com>
2020-10-18 15:16:32 +0800

    2020-10-18

--- 1614.括号的最大嵌套深度.cpp
+++ 1614.括号的最大嵌套深度.cpp
@@ -8,6 +8,19 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        //遇到左括号就++，遇到右括号就--
+        int ans=0;
+        int temp=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==')'){
+                temp--;
+            }else if(s[i]=='('){
+                //ans=max(ans,temp);
+                temp++;
+            }else ans=max(ans,temp); 
+        }
+        return ans;
+
 
     }
 };
