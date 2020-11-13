da661a7c110230896aa564a9ae2cab12d6c15d46
PrpgramTraveler <2115655391@qq.com>
2020-11-13 12:58:15 +0800

    2020-11-13

---
+++ 328.奇偶链表.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=cpp
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        ListNode* evenHead = head->next;
+        ListNode* odd = head;
+        ListNode* even = evenHead;
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
+
+// @lc code=end
+
