4796a0f125439da9deec1d8279ce8678db8b37a4
PrpgramTraveler <2115655391@qq.com>
2020-11-21 12:44:30 +0800

    2020-11-21

--- 148.排序链表.cpp
+++ 148.排序链表.cpp
@@ -18,39 +18,49 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-            
-        //如果链表为空，直接返回空链表
-        if(head==nullptr) return head;
-        //一个新的节点，用来指向头
-        ListNode* TempNode=new ListNode(0);
-        //指向头节点
-        TempNode->next=head;
-        //定义一个节点在头节点，一个在头节点之后
-        ListNode* LastNode=head;
-        ListNode* cur=head->next;
-        //开始对链表进行遍历
-        while(cur!=nullptr){
-            //如果当前的节点比后面的那个数小，就继续向后
-            if(LastNode->val<=cur->val){
-                LastNode=LastNode->next;
-            }else{
-                //否则的话就开始从头遍历，找到插入位置
-                ListNode* temp=TempNode;
-                while(temp->next->val<=cur->val){
-                    //cout<<"A"<<endl;
-                    temp=temp->next;
-                }
-                //维护前面的有序性
-                LastNode->next=cur->next;
-                cur->next=temp->next;
-                temp->next=cur;
-            }
-            //cur向后移动
-            cur=LastNode->next;
-        }
-        return TempNode->next;
+        return sortList(head, nullptr);
     }
 
+    ListNode* sortList(ListNode* head, ListNode* tail) {
+        if (head == nullptr) {
+            return head;
+        }
+        if (head->next == tail) {
+            head->next = nullptr;
+            return head;
+        }
+        ListNode* slow = head, *fast = head;
+        while (fast != tail) {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast != tail) {
+                fast = fast->next;
+            }
+        }
+        ListNode* mid = slow;
+        return merge(sortList(head, mid), sortList(mid, tail));
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* temp = dummyHead, *temp1 = head1, *temp2 = head2;
+        while (temp1 != nullptr && temp2 != nullptr) {
+            if (temp1->val <= temp2->val) {
+                temp->next = temp1;
+                temp1 = temp1->next;
+            } else {
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if (temp1 != nullptr) {
+            temp->next = temp1;
+        } else if (temp2 != nullptr) {
+            temp->next = temp2;
+        }
+        return dummyHead->next;
+    }
 };
 // @lc code=end
 
