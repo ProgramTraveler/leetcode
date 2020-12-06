895722a7033ec55c6e6a8c584ab681e01d078d17
PrpgramTraveler <2115655391@qq.com>
2020-12-06 16:12:50 +0800

    2020-12-06

---
+++ 118.杨辉三角.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=cpp
+ *
+ * [118] 杨辉三角
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        //很简但的数学方法
+        vector<vector<int>> ret(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            ret[i].resize(i + 1);
+            ret[i][0] = ret[i][i] = 1;
+            for (int j = 1; j < i; ++j) {
+                ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+            }
+        }
+        return ret;
+    }
+};
+
+// @lc code=end
+
