2f38b03d458f37b82639ba812e32a4c6ee18b684
PrpgramTraveler <2115655391@qq.com>
2020-12-19 10:04:33 +0800

    2020-12-19

---
+++ 48.旋转图像.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=cpp
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                //每次将旋转的点都处理一次
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+};
+
+// @lc code=end
+
