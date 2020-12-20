2d87990a71d9f86370201e7fad287e208234e60e
PrpgramTraveler <2115655391@qq.com>
2020-12-20 09:38:58 +0800

    2020-12-20

--- 316.去除重复字母.cpp
+++ 316.去除重复字母.cpp
@@ -7,52 +7,38 @@
 // @lc code=start
 class Solution {
 public:
+   class Solution {
+public:
     string removeDuplicateLetters(string s) {
-        stack<char> st; // 使用单调栈来记录。
-        vector<bool> visit(26, false); // 记录每个字符是否已在栈中
-        vector<int> charCount(26, 0); // 记录每个字符剩余的次数
-        //思路：维护一个单调递增栈，在每次出栈时增加一个原则：如果栈顶元素在后面已经没有了，就不能再出栈。
-        //入栈的原则：如果栈中已有该元素，则不入栈；否则入栈；
-
-        for (auto &a : s) {
-            charCount[a - 'a']++;
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            charCount[s[i] - 'a']--; // 注意更新元素的剩余次数
-
-            if (st.empty()) {
-                st.push(s[i]);
-                visit[s[i] - 'a'] = true; // 注意更新访问记录，第一次没过就是这里没更新
-                continue;
+        //记录s中的字母个数
+        vector<int> num(26);
+        //记录字母剩余个数
+        vector<int> sur(26);
+        //遍历s字符串，记录其中的字母个数
+        for (char x : s)
+            num[x - 'a'] ++;
+        //保存最终结果
+        string str;
+        //开始对s字符串进行处理
+    
+        for (char x : s) {
+            if (! sur[x - 'a']) {
+                while (! str.empty() && str.back() > x) {
+                    if (num[str.back() -'a'] > 0) {
+                        sur[str.back() - 'a'] = 0;
+                        str.pop_back();
+                    }else break;
+                }
+                sur[x - 'a'] = 1;
+                str.push_back(x);
             }
-
-            // 几个条件说明：!st.empty() && s[i] < st.top() -- 单调递增栈要求
-            // !visit[s[i] - 'a'] -- 如果当前元素已经在栈里了，肯定不用再入栈
-            // charCount[st.top() - 'a'] > 0 -- 如果栈顶元素，在剩余的数组里已经不再出现了，那就不能出栈（因为要求每个数字至少出现一次）
-            while (!st.empty() && s[i] < st.top() && !visit[s[i] - 'a'] && charCount[st.top() - 'a'] > 0) {
-                visit[st.top() - 'a'] = false; // 注意更新栈中的元素访问记录
-                st.pop();
-            }
-
-            if (!visit[s[i] - 'a']) {
-                st.push(s[i]);
-                visit[s[i] - 'a'] = true; // 注意更新栈中的元素访问记录
-            }
+            
+            num[x - 'a'] -= 1;
         }
-
-        string answer;
-        while (!st.empty()) {
-            answer += st.top();
-            st.pop();
-        }
-
-        reverse(answer.begin(), answer.end());
-
-        return answer;
-
+        return str;   
     }
 };
+
 
 
 // @lc code=end
