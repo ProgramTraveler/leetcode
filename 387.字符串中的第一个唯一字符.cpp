be2249837a51b8fed3757c849ca9bfbb78d336bc
PrpgramTraveler <2115655391@qq.com>
2020-12-23 14:16:15 +0800

    2020-12-23

--- 387.字符串中的第一个唯一字符.cpp
+++ 387.字符串中的第一个唯一字符.cpp
@@ -7,15 +7,17 @@
 // @lc code=start
 class Solution {
 public:
-    int firstUniqChar(string s) 
-    {
-        map<int,int> Map;//<关键字,关键字的值>（每个关键字在map里出现一次）
-        for(char x:s) Map[x]++; //记录关键字出现的次数
-        //查找只出现一次的关键字，并按题目要求返回
-        for(int i=0;i<s.length();i++)
-        {
-            if(Map[s[i]]==1) return i;
+    int firstUniqChar(string s) {
+        //设置一个数组；来记录子母个数
+        int ans[26] = {0};
+        //遍历字符串，记录字幕出现的个数
+        for (int i = 0; i < s.size(); i ++) 
+            ans[s[i] - 'a'] ++;
+        //创新遍历，如果有个数为1，直接返回结果
+        for (int i = 0; i < s.size(); i ++) {
+            if (ans[s[i] - 'a'] == 1) return i;
         }
+        //没有返回-1
         return -1;
     }
 };
