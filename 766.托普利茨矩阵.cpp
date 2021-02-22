409b5c444a35bd9a83e67ab876d1a7d21765a049
PrpgramTraveler <2115655391@qq.com>
2021-02-22 12:25:08 +0800

    2021-02-22

--- 766.托普利茨矩阵.cpp
+++ 766.托普利茨矩阵.cpp
@@ -11,6 +11,7 @@ public:
         //对矩阵中的元素进行遍历，只要当前数和它右下角元素相同说明满足条件
         for (int i = 0; i < matrix.size() - 1; i ++) { //最后一行和最后一列不用考虑
             for (int j = 0;j < matrix[0].size() - 1; j ++)
+                //不等就返回false
                 if (matrix[i][j] != matrix[i + 1][j + 1])
                     return false;
         }
