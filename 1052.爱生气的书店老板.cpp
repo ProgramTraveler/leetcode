2f524ccd2004a152014dba3b4b9dcdd201e11295
PrpgramTraveler <2115655391@qq.com>
2021-02-23 14:54:46 +0800

    2021-02-23

---
+++ 1052.爱生气的书店老板.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1052 lang=cpp
+ *
+ * [1052] 爱生气的书店老板
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
+        int total = 0;
+        int n = customers.size();
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                total += customers[i];
+            }
+        }
+        int increase = 0;
+        for (int i = 0; i < X; i++) {
+            increase += customers[i] * grumpy[i];
+        }
+        int maxIncrease = increase;
+        for (int i = X; i < n; i++) {
+            increase = increase - customers[i - X] * grumpy[i - X] + customers[i] * grumpy[i];
+            maxIncrease = max(maxIncrease, increase);
+        }
+        return total + maxIncrease;
+    }
+};
+// @lc code=end
+
