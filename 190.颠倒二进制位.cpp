7028d9897fc436651e37d010cd6b24379fec62ba
PrpgramTraveler <2115655391@qq.com>
2021-03-29 15:50:31 +0800

    2021-03-29

---
+++ 190.颠倒二进制位.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=190 lang=cpp
+ *
+ * [190] 颠倒二进制位
+ */
+
+// @lc code=start
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 32; ++i) {
+            res = (res << 1) | (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
