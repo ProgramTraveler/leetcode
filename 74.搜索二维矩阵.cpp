484510dfcbbc9a9e8042b03ca6a60a0a761aca3a
PrpgramTraveler <2115655391@qq.com>
2021-03-30 20:28:44 +0800

    2021-03-30

---
+++ 74.搜索二维矩阵.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=cpp
+ *
+ * [74] 搜索二维矩阵
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
+
