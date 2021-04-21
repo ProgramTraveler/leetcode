efa44633a858fbdc15063324822f6c8bef1590d5
PrpgramTraveler <2115655391@qq.com>
2021-04-21 13:44:36 +0800

    2021-04-21

---
+++ 91.解码方法.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=cpp
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        // a = f[i-2], b = f[i-1], c = f[i]
+        int a = 0, b = 1, c;
+        for (int i = 1; i <= n; ++i) {
+            c = 0;
+            if (s[i - 1] != '0') {
+                c += b;
+            }
+            if (i > 1 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + (s[i - 1] - '0') <= 26)) {
+                c += a;
+            }
+            tie(a, b) = {b, c};
+        }
+        return c;
+    }
+};
+// @lc code=end
+
