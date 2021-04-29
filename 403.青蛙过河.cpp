9b69200e0ebafd8ae0fb1d7e8cd0a7072f06e5bd
PrpgramTraveler <2115655391@qq.com>
2021-04-29 21:16:48 +0800

    2021-04-29

--- 403.青蛙过河.cpp
+++ 403.青蛙过河.cpp
@@ -7,8 +7,30 @@
 // @lc code=start
 class Solution {
 public:
-    bool canCross(vector<int>& stones) {
+    vector<unordered_map<int, int>> rec;
 
+    bool dfs(vector<int>& stones, int i, int lastDis) {
+        if (i == stones.size() - 1) {
+            return true;
+        }
+        if (rec[i].count(lastDis)) {
+            return rec[i][lastDis];
+        }
+        for (int curDis = lastDis - 1; curDis <= lastDis + 1; curDis++) {
+            if (curDis > 0) {
+                int j = lower_bound(stones.begin(), stones.end(), curDis + stones[i]) - stones.begin();
+                if (j != stones.size() && stones[j] == curDis + stones[i] && dfs(stones, j, curDis)) {
+                    return rec[i][lastDis] = true;
+                }
+            }
+        }
+        return rec[i][lastDis] = false;
+    }
+
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+        rec.resize(n);
+        return dfs(stones, 0, 0);
     }
 };
 // @lc code=end
