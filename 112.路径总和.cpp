2a2303a38c9a6ed80c6977700ae49f70089c10e1
PrpgramTraveler <2115655391@qq.com>
2021-05-11 10:30:57 +0800

    2021-05-11

--- 112.路径总和.cpp
+++ 112.路径总和.cpp
@@ -16,8 +16,15 @@
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int sum) {
-        
+    bool hasPathSum(TreeNode *root, int sum) {
+        if (root == nullptr) {
+            return false;
+        }
+        if (root->left == nullptr && root->right == nullptr) {
+            return sum == root->val;
+        }
+        return hasPathSum(root->left, sum - root->val) ||
+               hasPathSum(root->right, sum - root->val);
     }
 };
 // @lc code=end
