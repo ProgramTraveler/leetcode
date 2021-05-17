edc0dedf28a7f7dd98aa9d915609e82d55f5d77e
PrpgramTraveler <2115655391@qq.com>
2021-05-17 15:25:03 +0800

    2021-05-17

---
+++ 176.第二高的薪水.sql
@@ -0,0 +1,17 @@
+--
+-- @lc app=leetcode.cn id=176 lang=mysql
+--
+-- [176] 第二高的薪水
+--
+
+-- @lc code=start
+SELECT
+    (SELECT DISTINCT
+            Salary
+        FROM
+            Employee
+        ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+;
+-- @lc code=end
+
