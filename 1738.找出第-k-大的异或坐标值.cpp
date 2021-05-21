5d695e8663f539b99027183e4b586ed317ae1b59
PrpgramTraveler <2115655391@qq.com>
2021-05-21 10:09:04 +0800

    2021-05-22

---
+++ 1738.找出第-k-大的异或坐标值.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1738 lang=cpp
+ *
+ * [1738] 找出第 K 大的异或坐标值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int kthLargestValue(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> pre(m + 1, vector<int>(n + 1));
+        vector<int> results;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                pre[i][j] = pre[i - 1][j] ^ pre[i][j - 1] ^ pre[i - 1][j - 1] ^ matrix[i - 1][j - 1];
+                results.push_back(pre[i][j]);
+            }
+        }
+
+        sort(results.begin(), results.end(), greater<int>());
+        return results[k - 1];
+    }
+};
+// @lc code=end
+
