bba8a283376463fd409f5d3a02eff394dbcfa93b
PrpgramTraveler <2115655391@qq.com>
2021-05-29 08:38:20 +0800

    2021-05-29

---
+++ 477.汉明距离总和.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=477 lang=cpp
+ *
+ * [477] 汉明距离总和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int totalHammingDistance(vector<int> &nums) {
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < 30; ++i) {
+            int c = 0;
+            for (int val : nums) {
+                c += (val >> i) & 1;
+            }
+            ans += c * (n - c);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
