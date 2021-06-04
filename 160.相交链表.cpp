5736a788c3975a7d31627f1e755eb853d2fa2dc1
PrpgramTraveler <2115655391@qq.com>
2021-06-04 13:27:46 +0800

    2021-06-04

--- 160.相交链表.cpp
+++ 160.相交链表.cpp
@@ -16,25 +16,21 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (headA == nullptr || headB == nullptr) return nullptr;
-        
+        if (headA == nullptr || headB == nullptr) return nullptr; //如果其中一个链表为空，直接返回空
+        //定义两个初始节点，感觉和快慢指针有点像
         ListNode* head1 = headA;
         ListNode* head2 = headB;
 
-        while (head1 != head2) {
-            if (head1 != nullptr) {
+        while (head1 != head2) { //循环开始条件，当两个节点不相同时
+            if (head1 != nullptr) 
                 head1 = head1 -> next;
-            }else {
-                head1 = headB;
-            }
+            else head1 = headB;
 
-            if (head2 != nullptr) {
+            if (head2 != nullptr)
                 head2 = head2 -> next;
-            }else {
-                head2 = headA;
-            }
+            else head2 = headA;
         }
-        return head2;
+        return head1;
     }
 };
 // @lc code=end
