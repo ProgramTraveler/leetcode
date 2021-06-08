33307434299cc3ec6c16fca5a34499b4eba1d2e9
PrpgramTraveler <2115655391@qq.com>
2021-06-08 12:41:13 +0800

    2021-06-08

--- 203.移除链表元素.cpp
+++ 203.移除链表元素.cpp
@@ -18,17 +18,17 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* due = new ListNode(-1);
-        ListNode* p = head;
-        ListNode* q = due;
+        ListNode* due = new ListNode(-1); //设置一个虚拟节点，用来处理当头节点为目标值时的情况
+        ListNode* p = head; //指向头节点
+        ListNode* q = due; //指向虚拟节点
         while (p != nullptr) {
-            if (p -> val != val) {
-                q -> next = p;
-                q = q -> next;
+            if (p -> val != val) { //当前节点的值不是目标值
+                q -> next = p; //q指向当前节点
+                q = q -> next; //q指针后移一位
             }
-            p = p -> next;
+            p = p -> next; //当前节点是目标值的话，就跳过
         }
-        q -> next = nullptr;
+        q -> next = nullptr; //用来处理当目标值在最后的位置的情况
         return due -> next;
     }
 };
