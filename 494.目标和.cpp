f89d06cfe54724f844b0576c59394739f08eeead
PrpgramTraveler <2115655391@qq.com>
2021-06-08 12:39:26 +0800

    2021-06-08

---
+++ 494.目标和.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=cpp
+ *
+ * [494] 目标和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int count = 0;
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return count;
+    }
+
+    void backtrack(vector<int>& nums, int target, int index, int sum) {
+        if (index == nums.size()) {
+            if (sum == target) {
+                count++;
+            }
+        } else {
+            backtrack(nums, target, index + 1, sum + nums[index]);
+            backtrack(nums, target, index + 1, sum - nums[index]);
+        }
+    }
+};
+// @lc code=end
+
