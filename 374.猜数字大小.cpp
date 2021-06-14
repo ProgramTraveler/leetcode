b133fb79724e892eec51db95afc423229c11ef9f
PrpgramTraveler <2115655391@qq.com>
2021-06-14 12:23:44 +0800

    2021-06-14

---
+++ 374.猜数字大小.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=cpp
+ *
+ * [374] 猜数字大小
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 0, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) <= 0) {
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+// @lc code=end
+
