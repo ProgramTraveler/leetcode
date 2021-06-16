6d9d25c2237a21b27f988d2f970250eefca5839b
PrpgramTraveler <2115655391@qq.com>
2021-06-16 20:39:28 +0800

    2021-06-16

---
+++ 877.石子游戏.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=cpp
+ *
+ * [877] 石子游戏
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+// @lc code=end
+
