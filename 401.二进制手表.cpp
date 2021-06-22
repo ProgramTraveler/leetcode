16966e129cd4af5543ac3fc2e2f29d9def48703e
PrpgramTraveler <2115655391@qq.com>
2021-06-22 08:28:43 +0800

    2021-06-22

---
+++ 401.二进制手表.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=401 lang=cpp
+ *
+ * [401] 二进制手表
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    ans.push_back(to_string(h) + ":" + (m < 10 ? "0" : "") + to_string(m));
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
