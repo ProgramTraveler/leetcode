c953fc13ce2d9d6e53ec764432ee649bc7d94d84
PrpgramTraveler <2115655391@qq.com>
2021-07-14 09:23:04 +0800

    2021-07-14

--- 167.两数之和-ii-输入有序数组.cpp
+++ 167.两数之和-ii-输入有序数组.cpp
@@ -8,20 +8,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> ans;
+        vector<int> ans(2, 0);
         int left = 0, right = numbers.size() - 1;
         while (left < right) {
-            if (numbers[left] + numbers[right] > target)
-                right --;
-            else if (numbers[left] + numbers[right] < target) 
+            if (numbers[left] + numbers[right] < target) {
                 left ++;
-            else {
-                ans.push_back(left + 1);
-                ans.push_back(right + 1);
+            }else if (numbers[left] + numbers[right] > target) {
+                right --;
+            }else {
+                ans[0] = left + 1;
+                ans[1] = right + 1;
                 break;
             }
         }
         return ans;
+
     }
 };
 // @lc code=end
