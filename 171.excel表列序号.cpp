4147ce54d247cb53e948050f441997a8a99ba5a4
PrpgramTraveler <2115655391@qq.com>
2021-07-30 07:47:19 +0800

    2021-07-30

---
+++ 171.excel表列序号.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=cpp
+ *
+ * [171] Excel表列序号
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int number = 0;
+        long multiple = 1;
+        for (int i = columnTitle.size() - 1; i >= 0; i--) {
+            int k = columnTitle[i] - 'A' + 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
+    }
+};
+// @lc code=end
+
