3d64a5874339c6516646507bb13f508390bbb438
PrpgramTraveler <2115655391@qq.com>
2021-08-08 18:21:47 +0800

    2021-08-08

---
+++ 1137.第-n-个泰波那契数.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1137 lang=cpp
+ *
+ * [1137] 第 N 个泰波那契数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p = 0, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
+    }
+};
+// @lc code=end
+
