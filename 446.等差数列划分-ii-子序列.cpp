4c7c44384c06f7123ef96bf19f50c18ddd1569d3
PrpgramTraveler <2115655391@qq.com>
2021-08-11 18:50:53 +0800

    2021-08-11

---
+++ 446.等差数列划分-ii-子序列.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=446 lang=cpp
+ *
+ * [446] 等差数列划分 II - 子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int> &nums) {
+        int ans = 0;
+        int n = nums.size();
+        vector<unordered_map<long long, int>> f(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long long d = 1LL * nums[i] - nums[j];
+                auto it = f[j].find(d);
+                int cnt = it == f[j].end() ? 0 : it->second;
+                ans += cnt;
+                f[i][d] += cnt + 1;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
