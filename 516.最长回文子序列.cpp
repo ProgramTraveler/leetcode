bff2f2f8cdedb427c6ad40e514ef294d41e5a070
PrpgramTraveler <2115655391@qq.com>
2021-08-12 21:29:45 +0800

    2021-08-12

---
+++ 516.最长回文子序列.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=cpp
+ *
+ * [516] 最长回文子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            char c1 = s[i];
+            for (int j = i + 1; j < n; j++) {
+                char c2 = s[j];
+                if (c1 == c2) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
+// @lc code=end
+
