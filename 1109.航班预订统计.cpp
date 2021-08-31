4a532884a774571219cd8fd061b13358de0aac02
PrpgramTraveler <2115655391@qq.com>
2021-08-31 21:09:59 +0800

    2021-08-31

---
+++ 1109.航班预订统计.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1109 lang=cpp
+ *
+ * [1109] 航班预订统计
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> nums(n);
+        for (auto& booking : bookings) {
+            nums[booking[0] - 1] += booking[2];
+            if (booking[1] < n) {
+                nums[booking[1]] -= booking[2];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
+// @lc code=end
+
