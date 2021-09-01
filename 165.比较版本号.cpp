d37342d6d5418acad8078479fcc7906162aa4bc0
PrpgramTraveler <2115655391@qq.com>
2021-09-01 19:08:24 +0800

    2021-09-01

---
+++ 165.比较版本号.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=165 lang=cpp
+ *
+ * [165] 比较版本号
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1[i] != '.'; ++i) {
+                x = x * 10 + version1[i] - '0';
+            }
+            ++i; // 跳过点号
+            int y = 0;
+            for (; j < m && version2[j] != '.'; ++j) {
+                y = y * 10 + version2[j] - '0';
+            }
+            ++j; // 跳过点号
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
