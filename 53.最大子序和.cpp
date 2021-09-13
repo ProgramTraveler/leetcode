b5859756fd8ede3eaae1d2362fe585b10f969a47
PrpgramTraveler <2115655391@qq.com>
2021-09-13 17:43:42 +0800

    2021-09-13

--- 53.最大子序和.cpp
+++ 53.最大子序和.cpp
@@ -7,22 +7,23 @@
 // @lc code=start
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) 
-    {
-        int n=nums.size();
-        vector<int> dp(n,0);
-        dp[0]=nums[0];
-        int MaxSum=nums[0];
-        for(int i=1;i<n;i++)
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        /*if(n==1)
         {
-            dp[i]=max(nums[i]+dp[i-1],nums[i]);
-            MaxSum=A(dp[i],MaxSum);
+            return nums[0];
+        }*/
+        vector<int> dp(n,0);
+        dp[0] = nums[0];
+        int MaxSum = nums[0];
+        for(int i = 1; i < n; i ++) {
+            dp[i] = max(nums[i] + dp[i - 1], nums[i]);
+            MaxSum = A(dp[i], MaxSum);
         }
         return MaxSum;
     }
-    int A(int a,int b)
-    {
-        if(a>b) return a;
+    int A(int a, int b) {
+        if(a > b) return a;
         else return b;
     }
 };
