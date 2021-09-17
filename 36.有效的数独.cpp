b1c57f1b86864ea423b93c50e270f9dc10afdeb3
PrpgramTraveler <2115655391@qq.com>
2021-09-17 14:22:09 +0800

    2021-09-17

---
+++ 36.有效的数独.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=36 lang=cpp
+ *
+ * [36] 有效的数独
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9][9];
+        int columns[9][9];
+        int subboxes[3][3][9];
+        memset(rows,0,sizeof(rows));
+        memset(columns,0,sizeof(columns));
+        memset(subboxes,0,sizeof(subboxes));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subboxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
