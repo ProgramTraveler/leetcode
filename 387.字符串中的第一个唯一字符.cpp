1918f752408263a96a34340d0b3b98581a499109
PrpgramTraveler <2115655391@qq.com>
2021-09-19 22:00:32 +0800

    2021-09-19

--- 387.字符串中的第一个唯一字符.cpp
+++ 387.字符串中的第一个唯一字符.cpp
@@ -8,16 +8,14 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        //设置一个数组；来记录子母个数
-        int ans[26] = {0};
-        //遍历字符串，记录字幕出现的个数
-        for (int i = 0; i < s.size(); i ++) 
-            ans[s[i] - 'a'] ++;
-        //创新遍历，如果有个数为1，直接返回结果
-        for (int i = 0; i < s.size(); i ++) {
-            if (ans[s[i] - 'a'] == 1) return i;
+        //因为全是小写，建立一个26个字母的数组
+        int ap[26] = {0}; 
+        for (int i = 0; i < s.size(); i ++) //记录出现的次数
+            ap[s[i] - 'a'] ++;
+        for (int i = 0; i < s.size(); i ++) { //按顺序查找，返回第一个出现的字母下标
+            if (ap[s[i] - 'a'] == 1)
+                return i; 
         }
-        //没有返回-1
         return -1;
     }
 };
