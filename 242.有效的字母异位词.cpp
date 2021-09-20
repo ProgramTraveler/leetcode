2f1587ac18b86f1de8394cfd27dd2da0558a1419
PrpgramTraveler <2115655391@qq.com>
2021-09-20 19:41:58 +0800

    2021-09-20

--- 242.有效的字母异位词.cpp
+++ 242.有效的字母异位词.cpp
@@ -19,24 +19,16 @@ public:
         return true;*/
         //换种方法
         //如果两个字符串的长度不一样，直接返回false
-        if(s.size()!=t.size()) return false;
-        //定义一个26个大小的数组，用来记录26个小写字母
-        int num[26]={0};
-        //比较s和t中的字母
-        for(int i=0;i<s.size();i++){
-            //将s中的字母对应的位置加一
-            num[s[i]-'a']++;
-            //将t中的字母对应的位置减一
-            num[t[i]-'a']--;
-        }
-        //遍历num[]中的数字，观察是否为0
-        for(int i=0;i<26;i++){
-            //为0说明要么不存在，要么s和t中都有
-            if(num[i]==0) continue;
-            //不为0就直接返回false
+       if (s.size() != t.size()) return false;
+        int aps[26] = {0}, apt[26] = {0};
+        for (int i = 0; i < s.size(); i ++)
+            aps[s[i] - 'a'] ++;
+        for (int i = 0; i < t.size(); i ++)
+            apt[t[i] - 'a'] ++;
+        for (int i = 0; i < 26; i ++) {
+            if (aps[i] == apt[i]) continue;
             else return false;
         }
-        //全部遍历完，都为0返回true
         return true;
     }
 };
