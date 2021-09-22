ccc4d5589ea34084951c250b464563167b5783a0
PrpgramTraveler <2115655391@qq.com>
2021-09-22 15:07:47 +0800

    2021-09-22

--- 21.合并两个有序链表.cpp
+++ 21.合并两个有序链表.cpp
@@ -29,26 +29,26 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
 
-        ListNode* head=new ListNode(-1); //新链表的头节点
-        ListNode* pre=head; 
+        ListNode* head = new ListNode(-1); //新链表的头节点
+        ListNode* pre = head; 
         //迭代停止条件
-        while(l1!=nullptr && l2!=nullptr){
+        while(l1 != nullptr && l2 != nullptr) {
             //如果l1的节点值比l2得我节点值大
-            if(l1->val>l2->val){
+            if(l1 -> val > l2 -> val) {
                 //pre节点就指向l2，更新l2得节点
-                pre->next=l2;
-                l2=l2->next;
+                pre -> next = l2;
+                l2 = l2 -> next;
             }
-            else{
-                pre->next=l1;
-                l1=l1->next;
+            else {
+                pre -> next = l1;
+                l1 = l1 -> next;
             }
 
-            pre=pre->next;
+            pre = pre -> next;
         }
         //当循环停止后，l1或l2其中一个会是null
-        pre->next=l1==nullptr ? l2:l1;
-        return head->next;
+        pre -> next = l1 == nullptr ? l2 : l1;
+        return head -> next;
         
     }
 };
