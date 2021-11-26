89ca75f6c1874547629b94793b1cafbdd414058e
PrpgramTraveler <2115655391@qq.com>
2021-11-26 22:12:38 +0800

    2021-11-26

--- 700.二叉搜索树中的搜索.cpp
+++ 700.二叉搜索树中的搜索.cpp
@@ -26,23 +26,23 @@ public:
         else return root; //相等，就返回该节点*/
     
         //迭代的写法，老写递归没意思
-        TreeNode* node = root;
-
+       TreeNode* node = root;
         while (node) {
-            /*
+            /*这一步的思路有问题，按道理不应该是三个 if 连着写，这样会是同级判断，一路执行下去
             if (node -> val > val) node = node -> left;
             if (node -> val == val) return node;
             if (node -> val < val) node = node -> right;
             */
-            
+            //这样写才是正确的，分级执行
             if (node -> val == val) return node;
             else if (node -> val > val) node = node -> left;
             else node = node -> right;
 
+            //下面这样写更简洁，但执行过程是一样的
+            //if (node -> val == val) return node;
             //node = val < node -> val ? node -> left : node -> right;
         }
         return nullptr;
-    }
     }
 };
 // @lc code=end
