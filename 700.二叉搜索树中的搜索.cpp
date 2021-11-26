b9a258e50dcd1e922d389f860fa836ba248c6d37
PrpgramTraveler <2115655391@qq.com>
2021-11-26 22:07:47 +0800

    2021-11-26

--- 700.二叉搜索树中的搜索.cpp
+++ 700.二叉搜索树中的搜索.cpp
@@ -16,14 +16,14 @@
  */
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) 
-    {
-        if(root==NULL) return NULL; //为空，说明没有匹配的数，直接返回NULL
+    TreeNode* searchBST(TreeNode* root, int val) {
+        /*递归的写法，比较简单
+        if(root == NULL) return NULL; //为空，说明没有匹配的数，直接返回NULL
 
-        if(root->val<val) return searchBST(root->right,val); //如果当前数比给定值小，就去它的右子树搜索
-        else if(root->val>val) return searchBST(root->left,val);//如果当前数比给定值大，就去它的左子树搜索
+        if(root -> val < val) return searchBST(root -> right, val); //如果当前数比给定值小，就去它的右子树搜索
+        else if(root -> val > val) return searchBST(root -> left, val);//如果当前数比给定值大，就去它的左子树搜索
 
-        else return root; //相等，就返回该节点
+        else return root; //相等，就返回该节点*/
     }
 };
 // @lc code=end
