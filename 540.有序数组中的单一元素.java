05ed91f8d4fa29c76fc5ffe5cd2f4f8f20eba2b7
PrpgramTraveler <2115655391@qq.com>
2022-02-14 11:00:37 +0800

    2022-02-14

--- 540.有序数组中的单一元素.java
+++ 540.有序数组中的单一元素.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int ans = 0;
-        for (int i : nums) ans ^= i;
+        for (int i : nums) ans ^= i; //异或这个处理很nice
         return ans;
     }
 }
