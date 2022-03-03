76a31d73e997bdef409806a34d01b1b1f5cbb1c9
PrpgramTraveler <2115655391@qq.com>
2022-03-03 16:47:59 +0800

    2022-03-03

--- 258.各位相加.cpp
+++ 258.各位相加.cpp
@@ -7,25 +7,23 @@
 // @lc code=start
 class Solution {
 public:
-    int num_sum(int num)
-    {
-        if(num/10==0) return num; //如果这个数是个位数，就返回这个值
+    int num_sum(int num) {
+
+        if (num / 10 == 0) return num; //如果这个数是个位数，就返回这个值
         
-        int temp=0; //保存两位数相加的结果，作为新的num
+        int temp = 0; //保存两位数相加的结果，作为新的num
 
         //循环求解，把num的各个位数字相加
-        while(num>0)
-        {
-            temp+=num%10;
-            num=num/10;
+        while (num > 0) {
+            temp += num % 10;
+            num = num / 10;
         }
 
         //新数字作为新的num传入下一次的求和函数
-        num=num_sum(temp);
+        num = num_sum(temp);
         return num;
     }
-    int addDigits(int num) 
-    {
+    int addDigits(int num) {
         return num_sum(num);
     }
 };
