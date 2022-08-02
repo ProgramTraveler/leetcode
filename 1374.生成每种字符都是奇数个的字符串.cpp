9e36e9504fb041ce6d2c1dc8715640831415f811
PrpgramTraveler <2115655391@qq.com>
2022-08-02 10:19:56 +0800

    2022-08-02

---
+++ 1374.生成每种字符都是奇数个的字符串.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1374 lang=cpp
+ *
+ * [1374] 生成每种字符都是奇数个的字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string generateTheString(int n) { //好呆的题
+        if (n % 2 != 0) return string(n, 'a');
+        else return string(n - 1, 'a') + 'b';
+    }
+};
+// @lc code=end
+
