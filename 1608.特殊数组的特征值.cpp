31756fbad11d086e9158b85262e60bae3012b809
PrpgramTraveler <2115655391@qq.com>
2022-09-12 23:47:13 +0800

    2022-09-12

--- 1608.特殊数组的特征值.cpp
+++ 1608.特殊数组的特征值.cpp
@@ -5,6 +5,24 @@
  */
 
 // @lc code=start
+/*
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i <= 1000; i ++){
+            for(int j = 0; j < nums.size(); j ++){
+                if(nums[j] >= i) {
+                    count ++;
+                }
+            }
+            if(count == i) return i;
+            else count = 0;        
+        }
+        return -1;
+    }
+};*/
+
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
