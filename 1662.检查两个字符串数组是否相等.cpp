e6f4eb1a7d5cd5879e7febad0a4c8858c77b1a01
PrpgramTraveler <2115655391@qq.com>
2022-11-04 11:05:01 +0800

    2022-11-04

--- 1662.检查两个字符串数组是否相等.cpp
+++ 1662.检查两个字符串数组是否相等.cpp
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+/*
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
@@ -14,6 +15,18 @@ public:
         for (int j = 0; j < word2.size(); j ++)
             b += word2[j];
         return a==b;
+    }
+};
+*/
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string res_1 = "", res_2 = "";
+
+        for (int i = 0; i < word1.size(); i ++) res_1 += word1[i];
+        for (int i = 0; i < word2.size(); i ++) res_2 += word2[i];
+
+        return res_1 == res_2;
     }
 };
 // @lc code=end
