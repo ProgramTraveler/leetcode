5b71ad2c1edb9d960bca7781987b6285a7c39ad4
PrpgramTraveler <2115655391@qq.com>
2023-03-21 16:33:53 +0800

    2023-03-21

--- Leetcode_Question/107.二叉树的层序遍历-ii.cpp
+++ Leetcode_Question/107.二叉树的层序遍历-ii.cpp
@@ -19,7 +19,43 @@
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<TreeNode*> qu;
+        vector<TreeNode*> tem;
+        
+        vector<vector<int>> res;
+        vector<int> cnt;
 
+        if (!root) return res;
+
+
+        qu.push_back(root);
+        // cnt.push_back(root -> val);
+
+        // res.push_back(cnt);
+
+        // cnt.clear();
+
+        while (qu.size() != 0) {
+            tem = qu;
+
+            qu.clear();
+
+            for (int i = 0; i < tem.size(); i ++) {
+                if (tem[i] -> left != nullptr) qu.push_back(tem[i] -> left);
+
+                if (tem[i] -> right != nullptr) qu.push_back(tem[i] -> right);
+
+                cnt.push_back(tem[i] -> val);
+            }
+
+            res.push_back(cnt);
+
+            cnt.clear();
+        }
+
+        reverse(res.begin(), res.end());
+
+        return res;
     }
 };
 // @lc code=end
