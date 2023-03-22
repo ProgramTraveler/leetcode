94bc7cffc65b3cba808542da3a00c41b132696c8
PrpgramTraveler <2115655391@qq.com>
2023-03-22 10:44:59 +0800

    2023-03-22

--- Leetcode_Question/1626.无矛盾的最佳球队.cpp
+++ Leetcode_Question/1626.无矛盾的最佳球队.cpp
@@ -8,6 +8,38 @@
 class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        // 我记得好像是周赛题
+
+        int n = scores.size();
+
+        pair<int, int> a[n]; // 这个没怎么写过
+
+        for (int i = 0; i < n; i ++) a[i] = {scores[i], ages[i]};
+
+        // 这样的排序
+        sort(a, a + n);
+
+        int f[n];
+        memset(f, 0, sizeof(f));
+
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < i; j ++) {
+                if (a[j].second <=a[i].second)  // 比较年龄 因为分数已经按从小到大排序了
+                    f[i] = max(f[i], f[j]);
+                
+            }
+
+            f[i] += a[i].first;
+        }
+
+        // 返回数组中最大的数字
+        return *max_element(f, f + n);
+
+    }
+};
+/*class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n = scores.size();
         vector<int> order(n);
         for (int i = 0; i < n; ++i)
@@ -29,6 +61,6 @@ public:
         }
         return ans;
     }
-};
+};*/
 // @lc code=end
 
