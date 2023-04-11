90afb06035e0aef971449a4c2b482df6c041e155
PrpgramTraveler <2115655391@qq.com>
2023-04-11 15:20:12 +0800

    2023-04-11

---
+++ Leetcode_Question/1041.困于环中的机器人.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1041 lang=cpp
+ *
+ * [1041] 困于环中的机器人
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        vector<vector<int>> direc {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int direcIndex = 0;
+        int x = 0, y = 0;
+        for (char instruction : instructions) {
+            if (instruction == 'G') {
+                x += direc[direcIndex][0];
+                y += direc[direcIndex][1];
+            } else if (instruction == 'L') {
+                direcIndex += 3;
+                direcIndex %= 4;
+            } else {
+                direcIndex++;
+                direcIndex %= 4;
+            }
+        }
+        return direcIndex != 0 || (x == 0 && y == 0);
+    }
+};
+// @lc code=end
+
