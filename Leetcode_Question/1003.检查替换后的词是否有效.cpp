0dfddb391dad726d31111d1aaa63e9934ac13746
PrpgramTraveler <2115655391@qq.com>
2023-05-05 10:08:50 +0800

    2023-05-05

--- Leetcode_Question/1003.检查替换后的词是否有效.cpp
+++ Leetcode_Question/1003.检查替换后的词是否有效.cpp
@@ -7,8 +7,17 @@
 // @lc code=start
 class Solution {
 public:
-    bool isValid(string s) {
+    bool isValid(string s) { // s 同时作为栈
+        // 太巧妙了
+        int i = 0; // i - 1 表示栈顶下标 i = 0 表示栈为空
 
+        for (char c : s) {
+            if (c > 'a' && (i == 0 || c - s[-- i] != 1)) return false;
+
+            if (c < 'c') s[i ++] = c; // 入栈
+        }
+
+        return i == 0;
     }
 };
 // @lc code=end
