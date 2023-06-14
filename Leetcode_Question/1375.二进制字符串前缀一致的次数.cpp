a1fcf2c56dbf4e4eccf8c5643a2633d42dae8bcf
PrpgramTraveler <2115655391@qq.com>
2023-06-14 10:26:37 +0800

    2023-06-14

---
+++ Leetcode_Question/1375.二进制字符串前缀一致的次数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1375 lang=cpp
+ *
+ * [1375] 二进制字符串前缀一致的次数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numTimesAllBlue(vector<int>& flips) {
+        
+    }
+};
+// @lc code=end
+
