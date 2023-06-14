396290513d0f1b0cccb9fb1354c071a6fc3fab65
PrpgramTraveler <2115655391@qq.com>
2023-06-14 10:27:28 +0800

    2023-06-14

--- Leetcode_Question/1375.二进制字符串前缀一致的次数.cpp
+++ Leetcode_Question/1375.二进制字符串前缀一致的次数.cpp
@@ -5,10 +5,39 @@
  */
 
 // @lc code=start
-class Solution {
+/*class Solution {
 public:
     int numTimesAllBlue(vector<int>& flips) {
-        
+        // 记录 零 的最小位置的下标
+        // 关键是 怎么维持 零 的最小位置下标 -> 动态更新？ 每次 sort 感觉太费时
+        // 考虑一首 set?
+        // 时间复杂度还是太高了
+        int res = 0;
+
+        set<int> idx;
+
+        for (int i = 0; i < flips.size(); i ++) idx.insert(flips[i]);
+
+        for (int i = 0; i < flips.size(); i ++) {
+            idx.erase(flips[i]);
+
+            if (i + 1 < *idx.begin()) res ++;
+            
+        }
+
+        return res + 1; // 总会成功一次
+    }
+};*/
+
+class Solution {
+public:
+    int numTimesAllBlue(vector<int> &flips) {
+        int ans = 0, mx = 0, n = flips.size();
+        for (int i = 0; i < n; i++) {
+            mx = max(mx, flips[i]);
+            ans += mx == i + 1;
+        }
+        return ans;
     }
 };
 // @lc code=end
