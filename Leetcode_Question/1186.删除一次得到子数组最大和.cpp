2837d5c72d9586b36fd22f9c96277193acc6e75b
PrpgramTraveler <2115655391@qq.com>
2023-06-27 08:46:46 +0800

    2023-06-27

---
+++ Leetcode_Question/1186.删除一次得到子数组最大和.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1186 lang=cpp
+ *
+ * [1186] 删除一次得到子数组最大和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int dp0 = arr[0], dp1 = 0, res = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            dp1 = max(dp0, dp1 + arr[i]);
+            dp0 = max(dp0, 0) + arr[i];
+            res = max(res, max(dp0, dp1));
+        }
+        return res;
+    }
+};
+// @lc code=end
+
