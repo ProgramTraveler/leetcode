c79cabf2032e2c6dcefe325e2c934d85b611b37c
PrpgramTraveler <2115655391@qq.com>
2023-08-19 09:14:33 +0800

    2023-08-19

---
+++ Leetcode_Question/2235.两整数相加.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=2235 lang=cpp
+ *
+ * [2235] 两整数相加
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        // 搞笑的？
+        return num1 + num2;
+    }
+};
+// @lc code=end
+
