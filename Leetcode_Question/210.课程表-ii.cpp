ee2bb4253e604a953091781d4480d8ceabd606e2
PrpgramTraveler <2115655391@qq.com>
2023-09-11 09:59:47 +0800

    2023-09-11

---
+++ Leetcode_Question/210.课程表-ii.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=210 lang=cpp
+ *
+ * [210] 课程表 II
+ */
+
+// @lc code=start
+class Solution {
+private:
+    vector<vector<int>> edges;
+    vector<int> visited;
+    bool valid = true;
+    vector<int> res;
+
+public:
+    void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges[u]) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) { // 是否有环
+                    return;
+                }
+            }
+            else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+        res.emplace_back(u);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        visited.resize(numCourses);
+        for (const auto& info: prerequisites) {
+            edges[info[1]].push_back(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (!visited[i]) { // 课程没有修过
+                dfs(i);
+            }
+        }
+        if (!valid) return {};
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+// @lc code=end
+
