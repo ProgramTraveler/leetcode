86566a5310bba2e4a7992f4cc192ea7facc7388f
PrpgramTraveler <2115655391@qq.com>
2023-09-14 12:48:49 +0800

    2023-09-14

---
+++ Leetcode_Question/1222.可以攻击国王的皇后.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1222 lang=cpp
+ *
+ * [1222] 可以攻击国王的皇后
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
+
+    }
+};
+// @lc code=end
+
