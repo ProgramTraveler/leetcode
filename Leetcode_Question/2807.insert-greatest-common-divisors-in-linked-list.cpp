f9dcba5a30ac3d5df12e626a6c5a10a0033f8862
ProgramTraveler <2115655391@qq.com>
2024-01-06 19:55:24 +0800

    2024-01-06

---
+++ Leetcode_Question/2807.insert-greatest-common-divisors-in-linked-list.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=2807 lang=cpp
+ * @lcpr version=30112
+ *
+ * [2807] 在链表中插入最大公约数
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        // gcd
+        for (auto node = head; node->next != nullptr; node = node->next->next) {
+            node->next = new ListNode(gcd(node->val, node->next->val), node->next);
+        }
+
+        return head;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [18,6,10,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7]\n
+// @lcpr case=end
+
+ */
+
