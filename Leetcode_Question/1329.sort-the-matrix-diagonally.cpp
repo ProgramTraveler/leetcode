3bff3f9e7e9be956a00f988885eb7be2f6e51c6b
ProgramTraveler <2115655391@qq.com>
2024-04-29 20:10:18 +0800

    2024-04-29

---
+++ Leetcode_Question/1329.sort-the-matrix-diagonally.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=1329 lang=cpp
+ * @lcpr version=30122
+ *
+ * [1329] 将矩阵按对角线排序
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[3,3,1,1],[2,2,1,2],[1,1,1,2]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]]\n
+// @lcpr case=end
+
+ */
+
