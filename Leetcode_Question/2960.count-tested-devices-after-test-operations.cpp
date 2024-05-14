f26a34ceb2647f181be14d2d78b3ef82251e067f
ProgramTraveler <2115655391@qq.com>
2024-05-14 17:35:28 +0800

    2024-05-14

---
+++ Leetcode_Question/2960.count-tested-devices-after-test-operations.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=2960 lang=cpp
+ * @lcpr version=30122
+ *
+ * [2960] 统计已测试设备
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,1,2]\n
+// @lcpr case=end
+
+ */
+
