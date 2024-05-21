80edf125afaceae54b1de2c930c121fa02a356a3
ProgramTraveler <2115655391@qq.com>
2024-05-21 19:58:18 +0800

    2024-05-21

---
+++ Leetcode_Question/2769.找出最大的可达成数字.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2769 lang=cpp
+ *
+ * [2769] 找出最大的可达成数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        
+    }
+};
+// @lc code=end
+
